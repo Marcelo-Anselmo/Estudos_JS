01a741f891406cbfb4c67f8d8f1c835f4e59eeb6
Marcelo_Anselmo <121883911+Marcelo-Anselmo@users.noreply.github.com>
2023-08-15 21:40:39 -0300

    16_Rick_And_Mory_API
    Adicionei mais um projeto que fiz por conta própria, para treinar o consumo de API

---
+++ 16_Rick_and_Mory/js/script.js
@@ -0,0 +1,28 @@
+document.getElementById("card").style.setProperty("display", "none", "important") 
+
+function Procurar(){
+    document.getElementById("card").style.setProperty("display", "block", "important")
+
+    let person = document.getElementById("person").value
+    let finalURL = `https://rickandmortyapi.com/api/character/${person}`
+
+    fetch(finalURL)
+        .then(function(response){
+            return response.json()
+        })
+        .then(function(data){
+            let personImg = document.getElementById("personImg")
+            let personName = document.getElementById("personName")
+            let personGen = document.getElementById("personGen")
+            let personLoc = document.getElementById("personLoc")
+            let personStatus = document.getElementById("personStatus")
+            let personSpecie = document.getElementById("personSpecie")
+
+            personImg.src = data.image
+            personName.innerHTML = data.name
+            personGen.innerHTML = data.gender
+            personLoc.innerHTML = data.location
+            personStatus.innerHTML = data.status
+            personSpecie.innerHTML = data.species
+        })
+}
--- 1_Variaveis/variaveis01.js
+++ 1_Variaveis/variaveis01.js
@@ -1,3 +1,15 @@
+//Aqui esta uma das maneiras mais basicas de declarar uma variável
+
+//Quando se trata de declarar váriaveis temos 3 possibilidades mais comuns, let|const|var, cada uma com seu propósito.
+
+/*
+    let: Imagine que você tem uma caixinha que pode mudar de cor e de conteúdo sempre que você quiser. Um dia você pode colocar seus carrinhos nela, no outro dia suas bonecas, e assim por diante. Com o "let", você pode mudar o que tem dentro da caixinha quando quiser.
+
+    const: Agora, pense em outra caixinha que você só coloca algo uma vez e não pode mais trocar. Vamos dizer que você coloca sua pelúcia favorita nessa caixinha. Depois disso, não dá para tirar a pelúcia e colocar outro brinquedo no lugar. O "const" é como essa caixinha, você coloca algo nela e não pode mais mudar.
+
+    var: Por último, imagine uma caixinha um pouco mais velha e mais bagunçada. Às vezes, essa caixinha pode ter problemas e trocar o que tem dentro dela sem você perceber. Pode ser confuso! No JavaScript, "var" é como essa caixinha mais velha, ela não é tão cuidadosa em manter as coisas organizadas e pode causar problemas se não for usada com cuidado.
+
+*/
 var A1 = "Prisma"
 var A1 = "Corola"
 
